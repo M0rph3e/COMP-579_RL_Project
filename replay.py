7f8db692c5fb813c37260e7279408ada3bcbf1cb
Yassir Mamouni <yassirmamouni@MacBook-Air-de-Yassir.local>
2023-04-25 21:08:19 -0400

    can run with PG agent

--- replay.py
+++ replay.py
@@ -7,10 +7,11 @@ from gym.wrappers import FrameStack, GrayScaleObservation, TransformObservation
 from nes_py.wrappers import JoypadSpace
 
 from metrics import MetricLogger
-from agent import Mario
+from agent_ddqn import MarioDDQN
+from agent_pg import MarioPG
 from wrappers import ResizeObservation, SkipFrame
 
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
+env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
 
 env = JoypadSpace(
     env,
@@ -28,10 +29,13 @@ env.reset()
 
 save_dir = Path('checkpoints') / datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
 save_dir.mkdir(parents=True)
-file = 'trained_mario.chkpt'
+file = 'trained_mario_pg.pth'
 checkpoint = Path(file)
-mario = Mario(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir, checkpoint=checkpoint)
-mario.exploration_rate = mario.exploration_rate_min
+print(checkpoint,type(checkpoint))
+mario = MarioPG(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir, checkpoint=checkpoint)
+
+if mario.__class__.__name__ == "MarioDDQN":
+    mario.exploration_rate = mario.exploration_rate_min
 
 logger = MetricLogger(save_dir)
 
@@ -45,18 +49,27 @@ for e in range(episodes):
 
         env.render()
 
-        action = mario.act(state)#,model='target')
+        action = mario.act(state)
 
         next_state, reward, done, info = env.step(action)
+        #Episode Roll out depend on which agent is used
+        if mario.__class__.__name__ == "MarioDDQN":
+            mario.cache(state, next_state, action, reward, done)
+            logger.log_step(reward, None, None)
+            state = next_state
+            if done or info['flag_get']:
+                break
+        elif mario.__class__.__name__ == "MarioPG":
+            mario.cache(state, next_state, action, reward, done)
+            logger.log_step(reward, None, None)
+            state = next_state
+            if done or info['flag_get']:
+                break
+        elif mario.__class__.__name__ == "MarioPPO":
+            pass #TO DO
+        else:
+            raise ValueError(f"{mario.__class__.__name__} does not exist")
 
-        mario.cache(state, next_state, action, reward, done)
-
-        logger.log_step(reward, None, None)
-
-        state = next_state
-
-        if done or info['flag_get']:
-            break
 
     logger.log_episode()
 
