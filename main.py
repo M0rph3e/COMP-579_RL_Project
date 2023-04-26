8b3263a22b4ecce6689a9eca6f02405105a796b4
Yassir Mamouni <yassirmamouni@MacBook-Air-de-Yassir.local>
2023-04-25 21:08:11 -0400

    can run with PG agent

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ from gym.wrappers import FrameStack, GrayScaleObservation, TransformObservation
 from nes_py.wrappers import JoypadSpace
 
 from metrics import MetricLogger
-from agent import Mario
+from agent_ddqn import MarioDDQN
+from agent_pg import MarioPG
+#from agent_ppo import MarioPPO
 from wrappers import ResizeObservation, SkipFrame
 
 # Initialize Super Mario environment
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
+env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0')
 
 # Limit the action-space to
 #   0. walk right
@@ -38,7 +40,7 @@ save_dir = Path('checkpoints') / datetime.datetime.now().strftime('%Y-%m-%dT%H-%
 save_dir.mkdir(parents=True)
 
 checkpoint = None # Path('checkpoints/2020-10-21T18-25-27/mario.chkpt')
-mario = Mario(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir, checkpoint=checkpoint)
+mario = MarioPG(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir, checkpoint=checkpoint)
 
 logger = MetricLogger(save_dir)
 
@@ -48,7 +50,7 @@ episodes = 100
 for e in range(episodes):
 
     state = env.reset()
-
+    
     # Play the game!
     while True:
 
@@ -57,25 +59,37 @@ for e in range(episodes):
 
         # 4. Run agent on the state
         action = mario.act(state)
-
         # 5. Agent performs action
         next_state, reward, done, info = env.step(action)
+        
+        #Episode Roll out depend on which agent is used
+        if mario.__class__.__name__ == "MarioDDQN":
+            # 6. Remember
+            mario.cache(state, next_state, action, reward, done)
+            # 7. Learn
+            q, loss = mario.learn()
+            # 8. Logging
+            logger.log_step(reward, loss, q)
+            # 9. Update state
+            state = next_state
+            # 10. Check if end of game
+            if done or info['flag_get']:
+                break
+        elif mario.__class__.__name__ == "MarioPG":
+            # 6 Remember
+            mario.cache(state, next_state, action, reward, done)
 
-        # 6. Remember
-        mario.cache(state, next_state, action, reward, done)
+            # 7 Learn at the end of episode
+            if done or info['flag_get']:
+                q, loss = mario.learn()
+                logger.log_step(reward, q,loss)
+                break
 
-        # 7. Learn
-        q, loss = mario.learn()
+        elif mario.__class__.__name__ == "MarioPPO":
+            pass #TO DO
+        else:
+            raise ValueError(f"{mario.__class__.__name__} does not exist")
 
-        # 8. Logging
-        logger.log_step(reward, loss, q)
-
-        # 9. Update state
-        state = next_state
-
-        # 10. Check if end of game
-        if done or info['flag_get']:
-            break
 
     logger.log_episode()
 
